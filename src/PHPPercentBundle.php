2477dcfa64dc79581451cca31ddfddac89e4e973
Guillaume Wambre <guillaume.wambre@assoconnect.com>
2020-09-15 17:30:53 +0200

    First commit

---
+++ src/PHPPercentBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\PHPPercentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PHPPercentBundle extends Bundle
+{
+
+}
---
+++ tests/Doctrine/DBAL/Types/PercentTypeTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\PHPPercentBundle\Tests\Doctrine\DBAL\Types;
+
+use AssoConnect\PHPPercent\Percent;
+use AssoConnect\PHPPercentBundle\Doctrine\DBAL\Types\PercentType;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class PercentTypeTest extends TestCase
+{
+    /**
+     * @var AbstractPlatform|MockObject
+     */
+    protected $platform;
+
+    /**
+     * @var Type
+     */
+    protected $type;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void
+    {
+        $this->platform = $this->getMockForAbstractClass(AbstractPlatform::class);
+        $this->type = new PercentType();
+
+        self::assertInstanceOf(Type::class, $this->type);
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @dataProvider invalidPHPValuesProvider
+     */
+    public function testInvalidTypeConversionToDatabaseValue($value): void
+    {
+        $this->expectException(ConversionException::class);
+
+        $this->type->convertToDatabaseValue($value, $this->platform);
+    }
+
+    public function testNullConversion(): void
+    {
+        self::assertNull($this->type->convertToPHPValue(null, $this->platform));
+    }
+
+    public function testIntegerConvertsToDatabaseValue(): void
+    {
+        self::assertIsInt($this->type->convertToDatabaseValue(new Percent(rand()), $this->platform));
+    }
+
+    public function testInvalidTypeConversionToPHPValue(): void
+    {
+        $this->expectException(ConversionException::class);
+
+        $this->type->convertToPHPValue('not an integer', $this->platform);
+    }
+
+    public function testConvertIntegerToPHPValue(): void
+    {
+        $int = rand();
+        $percent = new Percent($int);
+
+        self::assertTrue($this->type->convertToPHPValue($int, $this->platform)->equals($percent));
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public static function invalidPHPValuesProvider(): iterable
+    {
+        return [
+            [0],
+            [''],
+            ['foo'],
+            ['10:11:12'],
+            [new stdClass()],
+            [27],
+            [-1],
+            [1.2],
+            [[]],
+            [['an array']],
+        ];
+    }
+}
