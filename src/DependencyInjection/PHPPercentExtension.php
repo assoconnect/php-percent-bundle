0d87b77924b17c96910cfea5207619e685a20280
Sylvain Fabre <syl.fabre@gmail.com>
2022-02-14 10:01:13 +0100

    phpstan level 8 & php 8

--- src/DependencyInjection/PHPPercentExtension.php
+++ src/DependencyInjection/PHPPercentExtension.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace AssoConnect\PHPPercentBundle\DependencyInjection;
 
 use AssoConnect\PHPPercentBundle\Doctrine\DBAL\Types\PercentType;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class PHPPercentExtension extends Extension
 {
-    public function prepend(ContainerBuilder $container)
+    public function prepend(ContainerBuilder $container): void
     {
         $container->prependExtensionConfig(
             'doctrine',
@@ -29,7 +27,10 @@ class PHPPercentExtension extends Extension
         );
     }
 
-    public function load(array $configs, ContainerBuilder $container)
+    /**
+     * @param array<mixed> $configs
+     */
+    public function load(array $configs, ContainerBuilder $container): void
     {
     }
 }
--- src/PHPPercentBundle.php
+++ src/PHPPercentBundle.php
@@ -8,5 +8,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PHPPercentBundle extends Bundle
 {
-
 }
